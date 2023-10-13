ac2e34cbd7feca24c57e8d5e2d6ec5da54c49f79
guzmanalejandra <guz20262@uvg.edu.gt>
2023-10-13 17:06:44 -0600

    Avances x2

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ train_table = train_df.hvplot.table(columns=['id', 'keyword', 'location', 'text'
                                     sortable=True, selectable=True, width=700)
 
 layout = pn.Column(train_table)
-layout.servable()
+
+location_distribution = train_df['location'].value_counts().nlargest(20).hvplot.bar(title='Top 20 Locations', height=300, width=600)
+keyword_distribution = train_df['keyword'].value_counts().nlargest(20).hvplot.bar(title='Top 20 Keywords', height=300, width=600)
+
+# Panel layout
+exploration_panel = pn.Row(train_table, location_distribution, keyword_distribution)
+
+exploration_panel.servable()
